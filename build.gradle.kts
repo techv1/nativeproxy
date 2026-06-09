5f794f1c81a740fa3615f93020612cfbb47456c9
Vishnu Sharma <82498516+techv1@users.noreply.github.com>
2026-06-09 18:52:58 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
 plugins {
-    id("com.android.application") version "8.0.1" apply false
-    kotlin("android") version "1.8.21" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    id 'com.android.application' version '8.5.2' apply false
 }
